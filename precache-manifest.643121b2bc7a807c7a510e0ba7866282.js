4312a2c4a5d75252f2455ee5a344235355992b3d
rusakovic <maxrusakovich@gmail.com>
2019-12-18 19:09:09 +0300

    Updates

---
+++ precache-manifest.643121b2bc7a807c7a510e0ba7866282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c1134fdf09307e2defb4c2765d809c",
+    "url": "/async-table/index.html"
+  },
+  {
+    "revision": "3c6ab77e49556185b7b9",
+    "url": "/async-table/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "663c1a15d1c8e830ccc3",
+    "url": "/async-table/static/js/2.d7a2506d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/async-table/static/js/2.d7a2506d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c6ab77e49556185b7b9",
+    "url": "/async-table/static/js/main.b01e847d.chunk.js"
+  },
+  {
+    "revision": "b4814b2af5ba26fc15de",
+    "url": "/async-table/static/js/runtime-main.a240952c.js"
+  }
+]);
